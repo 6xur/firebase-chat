b9194058cb6fd9a8321e38c0afa755a5ced6a846
Robert Xu <Robert_xu2010@hotmail.com>
2022-07-19 20:58:41 +0930

    Add authentication

--- app/src/main/java/com/example/firebase_chat/RegisterUser.java
+++ app/src/main/java/com/example/firebase_chat/RegisterUser.java
@@ -1,5 +1,6 @@
 package com.example.firebase_chat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,8 +9,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.regex.Pattern;
 
 public class RegisterUser extends AppCompatActivity implements View.OnClickListener {
 
@@ -53,5 +62,63 @@ public class RegisterUser extends AppCompatActivity implements View.OnClickListe
         String name = editTextName.getText().toString().trim();
         String email = editTextEmail.getText().toString().trim();
         String password = editTextPassword.getText().toString().trim();
+
+        if(name.isEmpty()){
+            editTextName.setError("Name cannot be empty.");
+            editTextName.requestFocus();
+            return;
+        }
+
+        if(email.isEmpty()){
+            editTextEmail.setError("Email cannot be empty.");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()){
+            editTextPassword.setError("Password cannot be empty.");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if(password.length() < 6){
+            editTextPassword.setError("Password must be at least 6 characters long.");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if(!isValidEmailAddress(email)){
+            editTextEmail.setError("Please provide valid Email address.");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success,
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(RegisterUser.this, "Authentication succeeded.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(RegisterUser.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
+
+    public static boolean isValidEmailAddress(String email) {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+                "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                "A-Z]{2,7}$";
+        Pattern pattern = Pattern.compile(emailRegex);
+        return pattern.matcher(email).matches();
     }
 }
---
+++ app/src/main/java/com/example/firebase_chat/User.java
@@ -0,0 +1,15 @@
+package com.example.firebase_chat;
+
+public class User {
+
+    public String name, email;
+
+    public User(){
+
+    }
+
+    public User(String name, String email){
+        this.name = name;
+        this.email = email;
+    }
+}
