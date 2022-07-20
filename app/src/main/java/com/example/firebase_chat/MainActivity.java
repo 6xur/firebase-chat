0d095d67d0f5eeb4bb45a2b39087e6f2702c625b
Robert Xu <Robert_xu2010@hotmail.com>
2022-07-20 12:03:50 +0930

    Implement login

--- app/src/main/java/com/example/firebase_chat/MainActivity.java
+++ app/src/main/java/com/example/firebase_chat/MainActivity.java
@@ -1,23 +1,35 @@
 package com.example.firebase_chat;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private EditText editTextEmail, editTextPassword;
+    private Button login;
     private TextView register;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        register = findViewById(R.id.register);
-        register.setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();
+
+        editTextEmail = findViewById(R.id.email);
+        editTextPassword = findViewById(R.id.password);
+
+        login = findViewById(R.id.login);
+        login.setOnClickListener(this);
     }
 
     @Override
@@ -26,6 +38,47 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.register:
                 startActivity(new Intent(this, RegisterUser.class));
                 break;
+            case R.id.login:
+                userLogin();
+                break;
         }
+    }
+
+    private void userLogin() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if(email.isEmpty()){
+            editTextEmail.setError("Email cannot be empty.");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()){
+            editTextPassword.setError("Password cannot be empty.");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if(password.length() < 6){
+            editTextPassword.setError("Password must be at least 6 characters long.");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if(!RegisterUser.isValidEmailAddress(email)){
+            editTextEmail.setError("Please provide valid Email address.");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+            if(task.isSuccessful()){
+                startActivity(new Intent(MainActivity.this, HomeActivity.class));
+                // finish();
+            } else{
+                Toast.makeText(MainActivity.this, "Failed to login, please check your credentials.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
