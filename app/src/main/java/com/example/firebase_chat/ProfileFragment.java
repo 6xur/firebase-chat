359a34e04b7fbea52a0d60739cc7615ac0d895a2
Robert Xu <Robert_xu2010@hotmail.com>
2022-07-23 16:07:32 +1000

    Implement profile image selection

--- app/src/main/java/com/example/firebase_chat/ProfileFragment.java
+++ app/src/main/java/com/example/firebase_chat/ProfileFragment.java
@@ -1,14 +1,26 @@
 package com.example.firebase_chat;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
-public class ProfileFragment extends Fragment {
+public class ProfileFragment extends Fragment implements View.OnClickListener {
+
+    Uri imageUri;
+    ImageView profilePicture;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -17,7 +29,38 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        profilePicture = view.findViewById(R.id.profilePicture);
+        profilePicture.setOnClickListener(this);
+
+        return view;
     }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.profilePicture:
+                choosePicture();
+        }
+    }
+
+    private void choosePicture() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        ChoosePictureLauncher.launch(intent);
+    }
+
+    ActivityResultLauncher<Intent> ChoosePictureLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if(result.getResultCode() == RESULT_OK){
+                        imageUri = result.getData().getData();
+                        profilePicture.setImageURI(imageUri);
+                    }
+                }
+            });
 }
