b59e083211bfcd9d3ea7319e1992560214c1e212
Robert Xu <u7279920@anu.edu.au>
2022-12-05 12:19:37 +1100

    Testing Scroll View

--- app/src/main/java/com/example/firebase_chat/HomeActivity.java
+++ app/src/main/java/com/example/firebase_chat/HomeActivity.java
@@ -1,12 +1,8 @@
 package com.example.firebase_chat;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.os.Bundle;
-
 import com.example.firebase_chat.databinding.ActivityHomeBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
