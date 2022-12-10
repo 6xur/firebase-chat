b2c29190109adaf22a475cc6c9753041ce876d7b
Robert Xu <u7279920@anu.edu.au>
2022-12-10 12:10:38 +1100

    Refactor UserDAO

--- app/src/main/java/com/example/firebase_chat/utilities/UserDao.java
+++ app/src/main/java/com/example/firebase_chat/utilities/UserDao.java
@@ -1,11 +1,11 @@
 package com.example.firebase_chat.utilities;
 
-import com.example.firebase_chat.utilities.Dao;
-import com.example.firebase_chat.utilities.User;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Objects;
 
 /**
  * The set method doesn't exist because Firebase loads and synchronises data asynchronously.
@@ -14,20 +14,23 @@ import com.google.firebase.database.FirebaseDatabase;
 public class UserDao implements Dao<User> {
 
     private final DatabaseReference databaseRef;
+    String databaseUrl = "https://fir-chat-d5a58-default-rtdb.asia-southeast1.firebasedatabase.app";
 
     public UserDao() {
-        FirebaseDatabase database = FirebaseDatabase.getInstance("https://fir-chat-d5a58-default-rtdb.asia-southeast1.firebasedatabase.app");
+        FirebaseDatabase database = FirebaseDatabase.getInstance(databaseUrl);
         databaseRef = database.getReference("Users");
     }
 
     @Override
     public Task<Void> add(User user) {
-        return databaseRef.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(user);
+        return databaseRef.child(Objects.requireNonNull(FirebaseAuth.getInstance()
+                .getCurrentUser()).getUid()).setValue(user);
     }
 
     @Override
     public Task<Void> delete() {
-        return databaseRef.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).removeValue();
+        return databaseRef.child(Objects.requireNonNull(FirebaseAuth.getInstance()
+                .getCurrentUser()).getUid()).removeValue();
     }
 
     public DatabaseReference getDatabaseRef() {
