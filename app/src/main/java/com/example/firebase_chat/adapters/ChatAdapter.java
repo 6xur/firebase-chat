e4e185cb97d9c679077a1abef2265b9f00a04358
Robert Xu <u7279920@anu.edu.au>
2023-01-11 18:05:26 +1100

    Create ChatAdapter

---
+++ app/src/main/java/com/example/firebase_chat/adapters/ChatAdapter.java
@@ -0,0 +1,102 @@
+package com.example.firebase_chat.adapters;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.firebase_chat.databinding.ItemContainerReceivedMessageBinding;
+import com.example.firebase_chat.databinding.ItemContainerSentMessageBinding;
+import com.example.firebase_chat.utilities.Message;
+
+import java.util.List;
+
+public class ChatAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private final List<Message> messages;
+    private final String senderUid;
+
+    public static final int VIEW_TYPE_SENT = 1;
+    public static final int VIEW_TYPE_RECEIVED = 2;
+
+    public ChatAdapter(List<Message> messages, String senderUid) {
+        this.messages = messages;
+        this.senderUid = senderUid;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if(viewType == VIEW_TYPE_SENT) {
+            return new SentMessageViewHolder(
+                    ItemContainerSentMessageBinding.inflate(
+                            LayoutInflater.from(parent.getContext()),
+                            parent,
+                            false
+                    )
+            );
+        } else {
+            return new ReceivedMessageViewHolder(
+                    ItemContainerReceivedMessageBinding.inflate(
+                            LayoutInflater.from(parent.getContext()),
+                            parent,
+                            false
+                    )
+            );
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if(getItemViewType(position) == VIEW_TYPE_SENT) {
+            ((SentMessageViewHolder) holder).setData(messages.get(position));
+        } else {
+            ((ReceivedMessageViewHolder) holder).setData(messages.get(position));
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return messages.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if(messages.get(position).senderUid.equals(senderUid)) {
+            return VIEW_TYPE_SENT;
+        } else {
+            return VIEW_TYPE_RECEIVED;
+        }
+    }
+
+    static class SentMessageViewHolder extends RecyclerView.ViewHolder {
+
+        private final ItemContainerSentMessageBinding binding;
+
+        SentMessageViewHolder(ItemContainerSentMessageBinding itemContainerSentMessageBinding) {
+            super(itemContainerSentMessageBinding.getRoot());
+            binding = itemContainerSentMessageBinding;
+        }
+
+        void setData(Message message) {
+            binding.textMessage.setText(message.message);
+            binding.textDateTime.setText(message.timestamp);
+        }
+    }
+
+    static class ReceivedMessageViewHolder extends RecyclerView.ViewHolder {
+
+        private final ItemContainerReceivedMessageBinding binding;
+
+        ReceivedMessageViewHolder(ItemContainerReceivedMessageBinding itemContainerReceivedMessageBinding) {
+            super(itemContainerReceivedMessageBinding.getRoot());
+            binding = itemContainerReceivedMessageBinding;
+        }
+
+        void setData(Message message) {
+            binding.textMessage.setText(message.message);
+            binding.textMessage.setText(message.timestamp);
+        }
+    }
+}
