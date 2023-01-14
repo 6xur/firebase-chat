fc58c0f1c3e7149702719cd062998d161a8392d2
Robert Xu <u7279920@anu.edu.au>
2023-01-14 16:53:02 +1100

    Add Chat fragment

--- app/src/main/java/com/example/firebase_chat/activities/HomeActivity.java
+++ app/src/main/java/com/example/firebase_chat/activities/HomeActivity.java
@@ -5,6 +5,7 @@ import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
+import com.example.firebase_chat.fragments.ChatFragment;
 import com.example.firebase_chat.fragments.UsersFragment;
 import com.example.firebase_chat.fragments.ProfileFragment;
 import com.example.firebase_chat.R;
@@ -17,19 +18,23 @@ public class HomeActivity extends AppCompatActivity {
     ActivityHomeBinding binding;
     BottomNavigationView bottomNavigationView;
 
-    public static final int USERS_FRAGMENT = 0;
-    public static final int PROFILE_FRAGMENT = 1;
+    public static final int CHAT_FRAGMENT = 0;
+    public static final int USERS_FRAGMENT = 1;
+    public static final int PROFILE_FRAGMENT = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_home);
-        replaceFragment(USERS_FRAGMENT);
+        replaceFragment(CHAT_FRAGMENT);
 
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setOnItemSelectedListener(item -> {
             switch (item.getItemId()) {
+                case R.id.chat:
+                    replaceFragment(CHAT_FRAGMENT);
+                    break;
                 case R.id.users:
                     replaceFragment(USERS_FRAGMENT);
                     break;
@@ -43,8 +48,9 @@ public class HomeActivity extends AppCompatActivity {
 
     /**
      * Switch whatever is in the frame layout with the new fragment
-     * 0 - Users Fragment
-     * 1 - Profile Fragment
+     * 0 - Chat fragment
+     * 1 - Users fragment
+     * 2 - Profile fragment
      **/
     private void replaceFragment(int position) {
         FragmentManager fragmentManager = getSupportFragmentManager();
@@ -62,6 +68,10 @@ public class HomeActivity extends AppCompatActivity {
                     // If the other fragment is visible, hide it
                     fragmentManager.beginTransaction().hide(fragmentManager.findFragmentByTag(Constants.KEY_PROFILE)).commit();
                 }
+                if (fragmentManager.findFragmentByTag("chat") != null) {
+                    // If the other fragment is visible, hide it
+                    fragmentManager.beginTransaction().hide(fragmentManager.findFragmentByTag("chat")).commit();
+                }
                 break;
             case PROFILE_FRAGMENT:
                 if (fragmentManager.findFragmentByTag(Constants.KEY_PROFILE) != null) {
@@ -72,7 +82,23 @@ public class HomeActivity extends AppCompatActivity {
                 if (fragmentManager.findFragmentByTag(Constants.KEY_USERS) != null) {
                     fragmentManager.beginTransaction().hide(fragmentManager.findFragmentByTag(Constants.KEY_USERS)).commit();
                 }
+                if (fragmentManager.findFragmentByTag("chat") != null) {
+                    // If the other fragment is visible, hide it
+                    fragmentManager.beginTransaction().hide(fragmentManager.findFragmentByTag("chat")).commit();
+                }
                 break;
+            case CHAT_FRAGMENT:
+                if (fragmentManager.findFragmentByTag("chat") != null) {
+                    fragmentManager.beginTransaction().show(fragmentManager.findFragmentByTag("chat")).commit();
+                } else {
+                    fragmentManager.beginTransaction().add(R.id.frameLayout, new ChatFragment(), "chat").commit();
+                }
+                if (fragmentManager.findFragmentByTag(Constants.KEY_PROFILE) != null) {
+                    fragmentManager.beginTransaction().hide(fragmentManager.findFragmentByTag(Constants.KEY_PROFILE)).commit();
+                }
+                if (fragmentManager.findFragmentByTag(Constants.KEY_USERS) != null) {
+                    fragmentManager.beginTransaction().hide(fragmentManager.findFragmentByTag(Constants.KEY_USERS)).commit();
+                }
         }
     }
 }
